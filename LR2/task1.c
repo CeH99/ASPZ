54044d73ea4f048dcf3a4fbd6602ba79d4473d42
CeH9l <senya183381@gmail.com>
2026-03-07 14:42:59 +0000

    Final

--- LR2/task1.c
+++ LR2/task1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <time.h>
+#include <limits.h>
 
 int main() {
-    // calculating max of time_t
-    time_t max_time = ~((time_t)1 << (sizeof(time_t) * 8 - 1));
-    
+
     printf("Size of time_t: %zu bytes\n", sizeof(time_t));
-    
-    // trying to formate
-    struct tm *time_info = gmtime(&max_time);
-    
-    if (time_info == NULL) {
-        printf("Max time in sec: %lld\n", (long long)max_time);
-        printf("Date is to far in the future (about 292 billions of years), gmtime() cant formate it\n");
+
+    time_t max_time;
+
+    if (sizeof(time_t) == 4) {
+        max_time = 2147483647;
     } else {
-        printf("time_t will end in: %s", asctime(time_info));
+        max_time = 9223372036854775807LL;
     }
-    
+
+    printf("Max time_t value (seconds): %lld\n", (long long)max_time);
+    printf("Overflow date: %s", asctime(gmtime(&max_time)));
+
     return 0;
 }
