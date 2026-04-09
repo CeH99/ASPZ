bf9ba1f7b536e0402224cf7e1c6c1849954e7e88
CeH9l <senya183381@gmail.com>
2026-04-09 13:24:50 +0000

    asgsaghsa

--- LR3/MyTask.c
+++ LR3/MyTask.c
@@ -10,21 +10,18 @@ int main() {
 
     printf("Starting to open file descriptors...\n");
 
-    // Infinite loop to keep opening files until the system stops us
     while (1) {
-        // We open /dev/null because we just need to consume a file descriptor, 
-        // without actually writing data to the hard drive.
-        fd = open("/dev/null", O_RDONLY);
+        fd = open("/dev/null", O_RDONLY); // returns number (discriptor)
         
         if (fd < 0) {
-            // Check if the error is due to reaching the max open files limit (EMFILE)
+            
             if (errno == EMFILE) {
                 printf("\n[LIMIT REACHED]: Too many open files (EMFILE)!\n");
                 printf("Total file descriptors successfully opened by this process: %d\n", count);
             } else {
                 printf("\n[ERROR]: Failed to open file. Reason: %s\n", strerror(errno));
             }
-            break; // Exit the loop since we can't open any more files
+            break; 
         }
         count++;
     }
